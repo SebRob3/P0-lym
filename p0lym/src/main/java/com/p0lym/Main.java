655126eb3da06e92802d1234008f4db1909e5d31
dnlec <163060603+dnlec@users.noreply.github.com>
2025-02-12 08:04:34 -0500

    test lexer

--- p0lym/src/main/java/com/p0lym/Main.java
+++ p0lym/src/main/java/com/p0lym/Main.java
@@ -1,7 +1,43 @@
 package com.p0lym;
 
+import java.io.IOException;
+import java.io.StringReader;
+
+import com.p0lym.RobotLexer.Token;
+import com.p0lym.RobotLexer.TokenType;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String testProgram = "|nom x y one|\n" +
+                "proc putChips: n andBalloons: m [\n" +
+                "    |c b|\n" +
+                "    c := n .\n" + 
+                "    b := m .\n" + 
+                "    put : c ofType: #chips .  put: b ofType: #balloons ]\n" +
+                "\n" +
+                "proc goNorth [\n" +
+                "	 while: canMove: 1 inDir: #north do: [ move: 1 inDir: #north .\n" + 
+                "        ]\n" + 
+                "]\n" +
+                "\n" +
+                "\n" +
+                "proc goWest [\n" + 
+                "    if: canMove: 1 inDir: #west then: [move: 1 inDir: #west] else\n" + 
+                "       : [nop .]]\n" +
+                "\n" +
+                "[\n" + 
+                "    goTo: 3 with: 3 .\n" + 
+                "    putChips: 2 andBalloons: 1 .\n" + 
+                "]\n";
+
+    	RobotLexer lexer = new RobotLexer(new StringReader(testProgram));
+    	try {
+    		Token token;
+    		while ((token = lexer.nextToken()).getType() != TokenType.EOF) {
+    			System.out.println(token);
+    		}
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
     }
 }
